157f2014f52178f1ecc6cf7f2dec5267aa2e8dc6
leducphananh <83126186+leducphananh@users.noreply.github.com>
2021-05-10 09:21:12 +0700

    Add files via upload

---
+++ TicTacToeConstants.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author admin
+ */
+public interface TicTacToeConstants {
+    public static int PLAYER1 = 1;
+    public static int PLAYER2 = 2;
+    public static int PLAYER1_WON = 1;
+    public static int PLAYER2_WON = 2;
+    public static int DRAW = 3;
+    public static int CONTINUE = 4;
+    public static final int ROW = 10, COLUMN = 10;
+}
